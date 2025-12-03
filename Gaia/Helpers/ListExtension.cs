94ece9c33bb53f7b56edc1c05125207ac7b34a9a
gubard <maksimovsergey08@gmail.com>
2025-12-03 19:37:41 +0200

    Add helper methods and service interface

---
+++ Gaia/Helpers/ListExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace Gaia.Helpers;
+
+public static class ListExtension
+{
+    public static T AddItem<T>(this List<T> list, T item)
+    {
+        list.Add(item);
+
+        return item;
+    }
+}
---
+++ Gaia/Services/Service.cs
@@ -0,0 +1,7 @@
+﻿namespace Gaia.Services;
+
+public interface IService<in TGetRequest, in TPostRequest, TGetResponse, TPostResponse>
+{
+    public ValueTask<TGetResponse> GetAsync(TGetRequest request, CancellationToken ct);
+    public ValueTask<TPostResponse> PostAsync(TPostRequest request, CancellationToken ct);
+}
