2a763908fa5948436d37a2118dbbe6aab9f80e3f
gubard <maksimovsergey08@gmail.com>
2025-12-04 11:03:27 +0200

    Introduce abstract `Service` base class and helper for consistent HTTP communication

---
+++ Gaia/Helpers/RouteHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace Gaia.Helpers;
+
+public static class RouteHelper
+{
+    public const string Get = "/get";
+    public const string Post = "/post";
+    public const string GetName = "Get";
+    public const string PostName = "Post";
+}
--- Gaia/Services/Service.cs
+++ Gaia/Services/Service.cs
@@ -1,7 +1,39 @@
-﻿namespace Gaia.Services;
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using Gaia.Helpers;
+
+namespace Gaia.Services;
 
 public interface IService<in TGetRequest, in TPostRequest, TGetResponse, TPostResponse>
 {
-    public ValueTask<TGetResponse> GetAsync(TGetRequest request, CancellationToken ct);
-    public ValueTask<TPostResponse> PostAsync(TPostRequest request, CancellationToken ct);
+    ValueTask<TGetResponse> GetAsync(TGetRequest request, CancellationToken ct);
+    ValueTask<TPostResponse> PostAsync(TPostRequest request, CancellationToken ct);
+}
+
+public abstract class Service<TGetRequest, TPostRequest, TGetResponse, TPostResponse> : IService<TGetRequest, TPostRequest, TGetResponse, TPostResponse>
+{
+    private readonly HttpClient _httpClient;
+    private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+    protected Service(HttpClient httpClient, JsonSerializerOptions jsonSerializerOptions)
+    {
+        _httpClient = httpClient;
+        _jsonSerializerOptions = jsonSerializerOptions;
+    }
+
+    public async ValueTask<TGetResponse> GetAsync(TGetRequest request, CancellationToken ct)
+    {
+        using var httpResponse = await _httpClient.PostAsJsonAsync(RouteHelper.Get, request, _jsonSerializerOptions, ct);
+        var response = await httpResponse.Content.ReadFromJsonAsync<TGetResponse>(_jsonSerializerOptions, ct);
+
+        return response.ThrowIfNull();
+    }
+
+    public async ValueTask<TPostResponse> PostAsync(TPostRequest request, CancellationToken ct)
+    {
+        using var httpResponse = await _httpClient.PostAsJsonAsync(RouteHelper.Post, request, _jsonSerializerOptions, ct);
+        var response = await httpResponse.Content.ReadFromJsonAsync<TPostResponse>(_jsonSerializerOptions, ct);
+
+        return response.ThrowIfNull();
+    }
 }
