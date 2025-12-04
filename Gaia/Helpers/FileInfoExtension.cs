e4b22c4f933467c3aa2c4969aa32d8defb5ca6ac
gubard <maksimovsergey08@gmail.com>
2025-12-04 19:50:48 +0200

    Add `FileInfoExtension` with async read/write methods

---
+++ Gaia/Helpers/FileInfoExtension.cs
@@ -0,0 +1,18 @@
+﻿namespace Gaia.Helpers;
+
+public static class FileInfoExtension
+{
+    extension(FileInfo fileInfo)
+    {
+        public Task<string> ReadAllTextAsync()
+        {
+            return File.ReadAllTextAsync(fileInfo.FullName);
+        }
+
+        public Task WriteAllTextAsync(string text)
+        {
+            return File.WriteAllTextAsync(fileInfo.FullName, text);
+        }
+    }
+
+}
