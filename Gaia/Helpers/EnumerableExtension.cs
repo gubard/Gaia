9c82f7baba31591c6493827a14eb7e2832b62cfe
gubard <maksimovsergey08@gmail.com>
2025-12-15 18:48:37 +0200

    Add `ForEach` extension method to IEnumerable for streamlined iteration with actions

---
+++ Gaia/Helpers/EnumerableExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace Gaia.Helpers;
+
+public static class EnumerableExtension
+{
+    public static IEnumerable<T> ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+    {
+        foreach (var item in enumerable)
+        {
+            action(item);
+
+            yield return item;
+        }
+    }
+}
