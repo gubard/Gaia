f19b612e0bb3daec148ea42e0ccabcfa7c6e88dc
gubard <maksimovsergey08@gmail.com>
2025-12-16 20:48:52 +0200

    Add `Dis` disposable utility class to Gaia Models

---
+++ Gaia/Models/Dis.cs
@@ -0,0 +1,16 @@
+﻿namespace Gaia.Models;
+
+public class Dis : IDisposable
+{
+    private readonly Action _dispose;
+
+    public Dis(Action dispose)
+    {
+        _dispose = dispose;
+    }
+
+    public void Dispose()
+    {
+        _dispose.Invoke();
+    }
+}
