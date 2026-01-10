85c46b69907133362b68548b5b0b9a6c8b294b89
gubard <maksimovsergey08@gmail.com>
2026-01-10 10:58:40 +0200

    Add ObjectStorage and Serializer services for file-based persistence

---
+++ Gaia/Services/ObjectStorage.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.CompilerServices;
+using Gaia.Helpers;
+
+namespace Gaia.Services;
+
+public interface IObjectStorage
+{
+    ConfiguredValueTaskAwaitable<T?> LoadAsync<T>(string key, CancellationToken ct);
+    ConfiguredValueTaskAwaitable SaveAsync(string key, object obj, CancellationToken ct);
+}
+
+public sealed class FileObjectStorage : IObjectStorage
+{
+    public FileObjectStorage(DirectoryInfo directory, ISerializer serializer)
+    {
+        _directory = directory;
+        _serializer = serializer;
+    }
+
+    public ConfiguredValueTaskAwaitable<T?> LoadAsync<T>(string key, CancellationToken ct)
+    {
+        return LoadCore<T>(key, ct).ConfigureAwait(false);
+    }
+
+    public ConfiguredValueTaskAwaitable SaveAsync(string key, object obj, CancellationToken ct)
+    {
+        return SaveCore(key, obj, ct).ConfigureAwait(false);
+    }
+
+    private readonly DirectoryInfo _directory;
+    private readonly ISerializer _serializer;
+
+    private async ValueTask SaveCore(string key, object obj, CancellationToken ct)
+    {
+        var file = _directory.ToFile($"{key}.{_serializer.FileExtension}");
+
+        if (file.Exists)
+        {
+            file.Delete();
+        }
+
+        await using var stream = file.Create();
+        await _serializer.SerializeAsync(stream, obj, ct);
+    }
+
+    private async ValueTask<T?> LoadCore<T>(string key, CancellationToken ct)
+    {
+        var file = _directory.ToFile($"{key}.{_serializer.FileExtension}");
+
+        if (!file.Exists)
+        {
+            return default;
+        }
+
+        await using var stream = file.OpenRead();
+        var value = await _serializer.DeserializeAsync<T>(stream, ct);
+
+        return value;
+    }
+}
---
+++ Gaia/Services/Serializer.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.Json;
+
+namespace Gaia.Services;
+
+public interface ISerializer
+{
+    string FileExtension { get; }
+
+    ConfiguredValueTaskAwaitable<T?> DeserializeAsync<T>(Stream stream, CancellationToken ct);
+    ConfiguredValueTaskAwaitable SerializeAsync(Stream stream, object obj, CancellationToken ct);
+}
+
+public class JsonSerializer : ISerializer
+{
+    public JsonSerializer(JsonSerializerOptions options)
+    {
+        _options = options;
+    }
+
+    public string FileExtension => "json";
+
+    public ConfiguredValueTaskAwaitable<T?> DeserializeAsync<T>(Stream stream, CancellationToken ct)
+    {
+        return DeserializeCore<T>(stream, ct).ConfigureAwait(false);
+    }
+
+    public ConfiguredValueTaskAwaitable SerializeAsync(
+        Stream stream,
+        object obj,
+        CancellationToken ct
+    )
+    {
+        return SerializeCore(stream, obj, ct).ConfigureAwait(false);
+    }
+
+    private readonly JsonSerializerOptions _options;
+
+    private ValueTask<T?> DeserializeCore<T>(Stream stream, CancellationToken ct)
+    {
+        return System.Text.Json.JsonSerializer.DeserializeAsync<T>(stream, _options, ct);
+    }
+
+    private async ValueTask SerializeCore(Stream stream, object obj, CancellationToken ct)
+    {
+        await System.Text.Json.JsonSerializer.SerializeAsync(stream, obj, _options, ct);
+    }
+}
