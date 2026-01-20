2d5d1831f971f42e81c7559f202ccb73e92d4487
gubard <maksimovsergey08@gmail.com>
2026-01-20 16:08:21 +0200

    Add AutoDictionary utility class for managing keyed items with auto-creation

---
+++ Gaia/Models/Id.cs
@@ -0,0 +1,6 @@
+﻿namespace Gaia.Models;
+
+public interface IId<out T>
+{
+    public T Id { get; }
+}
---
+++ Gaia/Services/AutoDictionary.cs
@@ -0,0 +1,40 @@
+﻿using Gaia.Models;
+
+namespace Gaia.Services;
+
+public class AutoDictionary<TKey, TItem>
+    where TItem : IStaticFactory<TKey, TItem>, IId<TKey>
+    where TKey : notnull
+{
+    private readonly Dictionary<TKey, TItem> _items = new();
+
+    public TItem GetItem(TKey id)
+    {
+        if (_items.TryGetValue(id, out var value))
+        {
+            return value;
+        }
+
+        var result = TItem.Create(id);
+
+        if (_items.TryAdd(id, result))
+        {
+            return result;
+        }
+
+        return _items[id];
+    }
+
+    public void AddRange(IEnumerable<TItem> items)
+    {
+        foreach (var item in items)
+        {
+            _items.Add(item.Id, item);
+        }
+    }
+
+    public TItem[] ToItemsArray()
+    {
+        return _items.Values.ToArray();
+    }
+}
