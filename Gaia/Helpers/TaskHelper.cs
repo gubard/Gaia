2cde673a4cff8c79a2f6aba222314ebabdfc7690
Serhii Maksymov <maksimovsergey08@gmail.com>
2026-02-02 15:25:00 +0200

    Add cancellation token support to `TaskHelper` async methods

--- Gaia/Helpers/TaskHelper.cs
+++ Gaia/Helpers/TaskHelper.cs
@@ -13,17 +13,53 @@ public static class TaskHelper
     }
 
     public static ConfiguredValueTaskAwaitable WhenAllAsync(
-        params ConfiguredValueTaskAwaitable[] tasks
+        ConfiguredValueTaskAwaitable[] tasks,
+        CancellationToken ct
     )
     {
-        return WhenAllCore(tasks).ConfigureAwait(false);
+        return WhenAllCore(tasks, ct).ConfigureAwait(false);
     }
 
-    private static async ValueTask WhenAllCore(ConfiguredValueTaskAwaitable[] tasks)
+    private static async ValueTask WhenAllCore(
+        ConfiguredValueTaskAwaitable[] tasks,
+        CancellationToken ct
+    )
     {
         foreach (var task in tasks)
         {
+            ct.ThrowIfCancellationRequested();
+
             await task;
         }
+    }
+
+    public static ConfiguredValueTaskAwaitable<TResult[]> WhenAllAsync<TResult>(
+        ValueTask<TResult>[] tasks,
+        CancellationToken ct
+    )
+    {
+        if (tasks.Length == 0)
+        {
+            return FromResult(Array.Empty<TResult>());
+        }
+
+        return WhenAllCore(tasks, ct).ConfigureAwait(false);
+    }
+
+    private static async ValueTask<TResult[]> WhenAllCore<TResult>(
+        ValueTask<TResult>[] tasks,
+        CancellationToken ct
+    )
+    {
+        var result = new TResult[tasks.Length];
+
+        for (var index = 0; index < tasks.Length; index++)
+        {
+            ct.ThrowIfCancellationRequested();
+            var task = tasks[index];
+            result[index] = await task;
+        }
+
+        return result;
     }
 }
