33f640231bd005ad0a83abcdfbb85722ffd1a8f6
gubard <maksimovsergey08@gmail.com>
2026-03-10 11:32:04 +0200

    Add `OperationCanceled` warning log to `GaiaLog` for improved cancellation diagnostics

--- Gaia/Helpers/GaiaLog.cs
+++ Gaia/Helpers/GaiaLog.cs
@@ -6,4 +6,10 @@ public static partial class GaiaLog
 {
     [LoggerMessage(EventId = 1, Level = LogLevel.Error, Message = "Try {TryCount} exception")]
     public static partial void TryException(this ILogger logger, int tryCount, Exception exception);
+
+    [LoggerMessage(EventId = 2, Level = LogLevel.Warning, Message = "Operation canceled")]
+    public static partial void OperationCanceled(
+        this ILogger logger,
+        OperationCanceledException exception
+    );
 }
