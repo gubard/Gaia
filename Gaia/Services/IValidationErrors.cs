d4d5a5682962ea321994e6ce6000cce8423bab6c
gubard <maksimovsergey08@gmail.com>
2026-06-11 22:00:12 +0300

    Refactor `IValidationErrors` to add `EmptyValidationError` singleton and restructure `DefaultValidationErrors` implementation

--- Gaia/Services/IValidationErrors.cs
+++ Gaia/Services/IValidationErrors.cs
@@ -7,7 +7,14 @@ public interface IValidationErrors
     List<ValidationError> ValidationErrors { get; }
 }
 
+public sealed class EmptyValidationError : IValidationErrors
+{
+    public static IValidationErrors Instance = new EmptyValidationError();
+
+    public List<ValidationError> ValidationErrors => new();
+}
+
 public sealed class DefaultValidationErrors : IValidationErrors
 {
-    public List<ValidationError> ValidationErrors => new();
+    public List<ValidationError> ValidationErrors { get; } = new();
 }
