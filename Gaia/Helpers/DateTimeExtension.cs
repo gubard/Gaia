7f2cde073dc38b48cf2dde3b522ca95c3817b3d0
gubard <maksimovsergey08@gmail.com>
2026-08-03 13:01:57 +0300

    Add `MonthToUaString` extension method to format month names in Ukrainian

--- Gaia/Helpers/DateTimeExtension.cs
+++ Gaia/Helpers/DateTimeExtension.cs
@@ -1,9 +1,16 @@
-﻿namespace Gaia.Helpers;
+﻿using System.Globalization;
+
+namespace Gaia.Helpers;
 
 public static class DateTimeExtension
 {
     public static DateOnly ToDateOnly(this DateTime dateTime)
     {
         return DateOnly.FromDateTime(dateTime);
+    }
+
+    public static string MonthToUaString(this DateTime dateTime)
+    {
+        return dateTime.ToString("MMMM", new CultureInfo("uk-UA"));
     }
 }
